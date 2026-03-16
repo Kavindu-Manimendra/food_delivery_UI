a09ba48abbd105b983a8b956026ee21f742fbee1
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2026-03-16 11:55:06 +0530

    config added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 import ContactUs from "./pages/Contact Us/ContactUs.jsx";
 import ExploreFood from "./pages/ExploreFood/ExploreFood.jsx";
+import FoodDetails from "./pages/FoodDetails/FoodDetails.jsx";
+import Cart from "./pages/Cart/Cart.jsx";
 
 const App = () => {
     return (
@@ -13,6 +15,8 @@ const App = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/contact" element={<ContactUs />} />
                 <Route path="/explore" element={<ExploreFood />} />
+                <Route path="/food/:id" element={<FoodDetails />} />
+                <Route path="/cart" element={<Cart />} />
             </Routes>
         </div>
     );
