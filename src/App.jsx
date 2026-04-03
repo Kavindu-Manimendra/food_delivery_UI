c086bfe40bdd765419ae1d475df28f25f79c7fa7
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2026-04-03 12:29:13 +0530

    pages path added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 import ContactUs from "./pages/Contact Us/ContactUs.jsx";
 import ExploreFood from "./pages/ExploreFood/ExploreFood.jsx";
+import FoodDetails from "./pages/FoodDetails/FoodDetails.jsx";
+import Cart from "./pages/Cart/Cart.jsx";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder.jsx";
+import Login from "./pages/Login/Login.jsx";
+import Register from "./components/Register/Register.jsx";
 
 const App = () => {
     return (
@@ -13,6 +18,11 @@ const App = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/contact" element={<ContactUs />} />
                 <Route path="/explore" element={<ExploreFood />} />
+                <Route path="/food/:id" element={<FoodDetails />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/order" element={<PlaceOrder />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </div>
     );
